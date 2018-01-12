553da3664bce8a9773f120d9c7bf785dedd5d849
DESKTOP-DFLEMQ3\pc <chamseddine.bezzaouia@gmail.com>
2018-01-12 22:17:11 +0100

    add category 2.0

---
+++ src/DL/AchatBundle/Entity/Categorie.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 12/01/2018
+ * Time: 15:46
+ */
+
+namespace DL\AchatBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Categorie
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $libelle;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param mixed $libelle
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    }
+
+
+
+
+
+
+
+}
